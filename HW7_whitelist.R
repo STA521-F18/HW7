43fc6670f034dead6621c4c2d905ca7384a68485
merliseclyde <clyde@stat.duke.edu>
2018-11-13 21:09:42 -0500

    initial repo

---
+++ HW7_whitelist.R
@@ -0,0 +1,23 @@
+allowed_files = c("HW7.Rmd",
+                  "HW7.pdf",
+                  "HW7.html",
+                  "README.md",
+                  "HW7_whitelist.R",
+                  "diabetes.Rmd",
+                  "yX.diabetes.train.txt",
+                  "yX.diabetes.test.txt",
+                  "wercker.yml")
+
+files = dir()
+disallowed_files = files[!(files %in% allowed_files)]
+
+if (length(disallowed_files != 0))
+{
+  cat("Disallowed files found:\n")
+  cat("  (remove the following files from your repo)\n\n")
+
+  for(file in disallowed_files)
+    cat("*",file,"\n")
+
+  quit("no",1,FALSE)
+}
